c3797973f09a5a98726edc1af1757d5e5fd22062
nuwanthi-039 <86476550+nuwanthi-039@users.noreply.github.com>
2021-06-26 20:10:07 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1 >Javascript and React.js</h1>
+      <p >Javascript and React.js bootcanp is good .I was able to clear undastand about react.js .Thank you!</p>
+    </div>
+  );
+}
+export default Info;
